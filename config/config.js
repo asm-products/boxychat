f99e5e702e8d334d0dc0ccae15d0c7c0399d692c
Juan Corona <juan.carlos.corona.r@gmail.com>
2014-11-14 02:15:20 -0800

    Split configurations into tier files under a folder. Waterline configuration is now included along with application configuration.

--- config/config.js
+++ config/config.js
@@ -1,31 +1,27 @@
+// Configuration loader
+
+var env = process.env.NODE_ENV || 'development';
+
 var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    glob = require('glob');
 
-var config = {
-    development: {
-        root: rootPath,
-        app: {
-            name: 'Boxychat'
-        },
-        port: 3000
-    },
+var loadedConfig = {};
 
-    test: {
-        root: rootPath,
-        app: {
-            name: 'Boxychat'
-        },
-        port: 3000
-    },
+var configFiles = glob.sync(__dirname + '/tiers/*.js');
 
-    production: {
-        root: rootPath,
-        app: {
-            name: 'Boxychat'
-        },
-        port: 3000
+configFiles.forEach(function (config) {
+
+    var key = path.basename(config, '.js');
+
+    if (key === env) {
+        loadedConfig = require(config);
+        loadedConfig.env = env;
     }
-};
 
-module.exports = config[env];
+});
+
+if (Object.keys(loadedConfig).length === 0) {
+    console.error('Unable to load config file. Make sure an environment tier exists for:', env);
+}
+
+module.exports = loadedConfig;
