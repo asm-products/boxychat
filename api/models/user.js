e93c2d1aeaab6c1f40460d30c948e7ea830d0b06
Gonçalo Margalho <gsky89@gmail.com>
2014-11-16 01:05:08 +0100

    Add basic fields for a user and add a beforeCreate hook to crypt the password and generate a token

--- api/models/user.js
+++ api/models/user.js
@@ -4,6 +4,38 @@ module.exports = {
 
     attributes: {
         first_name: 'string',
-        last_name: 'string'
+        last_name: 'string',
+        email: {
+            type: 'email',
+            required: true,
+            unique: true
+        },
+        password: {
+            type: 'string',
+            required: true
+        },
+        activated: 'boolean',
+        activationToken: 'string'
+    },
+
+    /**
+     * Hash the users password with bcrypt
+     * @method beforeCreate
+     * @param {object}   user            the object of the submitted user data
+     * @param {Function} cb[err, user]   the callback to be used when bcrypts done
+     * @return
+     */
+    beforeCreate: function (user, cb) {
+        Service.crypt.generate({saltComplexity: 10}, user.password, function (err, hash) {
+            if (err) {
+                return cb(err);
+            } else {
+                user.password = hash;
+                user.activated = false;
+                user.activationToken = Service.crypt.token(new Date().getTime() + user.email);
+                return cb(null, user);
+            }
+        });
     }
+
 };
