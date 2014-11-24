24a8fee286d965c185e66d995518eb1627997db5
Juan Corona <juan.carlos.corona.r@gmail.com>
2014-11-23 21:59:30 -0800

    Improve chat controller.

--- api/controllers/chat.js
+++ api/controllers/chat.js
@@ -1,18 +1,27 @@
 module.exports = {
     model: 'chat',
     sockets: {
-        connected: function(){
+        connect: function() {
+            //TODO: not working for the moment
             console.log('user connected');
 
         },
-        disconnected :function(){
+        disconnect :function() {
+            //TODO: not working for the moment
             console.log('user disconnected');
         },
-        chatMessage: function(user, message){
-            console.log('%s: %s', user, message);
+        chatMessage: function (message) {
+            var that = this;
+            this.models.user.findOne(this.socket.user)
+                .then(function (user) {
+                    var out = '[' + user.firstName + '] ' + message;
+                    console.log(out);
+                    that.sockets.emit('chat:message', out);
+                });
         },
-        privateMessage: function(fromUser, toUser, message) {
-            console.log('%s>%s: %s', fromUser, toUser, message);
+        privateMessage: function (toUser, message) {
+            //TODO: needs a nice way of sending to a specific socket based on a user id, not socket id
+            console.log('%s>%s: %s', '', toUser, message);
         }
 
     }
