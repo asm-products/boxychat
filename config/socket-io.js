55ca7b0113a58e53481b355f2c1d380f5dcd62c1
Juan Corona <juan.carlos.corona.r@gmail.com>
2014-11-23 20:11:15 -0800

    Once again, get the token from the handshake query. (Not sure why in socket.io 1.0 they decided to move query to _query)

--- config/socket-io.js
+++ config/socket-io.js
@@ -5,7 +5,7 @@ module.exports = function(server, secrets) {
 
     io.use(function(socket, next) {
         var handshakeData = socket.request;
-        var token = handshakeData.query ? handshakeData.query.token : null;
+        var token = handshakeData._query ? handshakeData._query.token : null;
         Service.token.verify(token, secrets.token, function(err, decoded) {
             if (err) { return next(err); }
             if (!decoded) { return next(new Error('not authorized')); }
