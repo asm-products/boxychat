795bd4ade883515179c1b5241cbed093cd8a18b5
Juan Corona <juan.carlos.corona.r@gmail.com>
2014-11-23 21:39:08 -0800

    Better way to get query data from socket handshake.

--- config/socket-io.js
+++ config/socket-io.js
@@ -4,8 +4,7 @@ module.exports = function(server) {
     // Socket.io can be configured here before being loaded in app.js
 
     io.use(function(socket, next) {
-        var handshakeData = socket.request;
-        var token = handshakeData._query ? handshakeData._query.token : null;
+        var token = socket.handshake.query.token || null;
         Service.token.verify(token, function(err, decoded) {
             if (err) { return next(err); }
             if (!decoded) { return next(new Error('not authorized')); }
